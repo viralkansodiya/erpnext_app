91a8ef900c0cc2e28ec38a5b4feb96db7529a99d
viralkansodiya <viral@fosserp.com>
2025-07-18 16:20:49 +0530

    fix: amount was calculated wrong

--- custom_app/doc_events/quotation.py
+++ custom_app/doc_events/quotation.py
@@ -13,6 +13,7 @@ def validate(doc, method):
     for row in doc.items:
         if frappe.db.get_value("Item", row.get("item_code"), 'additional_service_item'):
             row.rate = total_amount * flt(row.percentage) / 100
+            row.amount = row.rate * row.qty
 
 def apply_item_tax_template(doc):
     max_tax = 0
@@ -57,3 +58,4 @@ def before_insert(self, method):
     for row in self.items:
         if frappe.db.get_value("Item", row.get("item_code"), 'additional_service_item'):
             row.rate = total_amount * flt(row.percentage) / 100
+            row.amount = row.rate * row.qty
