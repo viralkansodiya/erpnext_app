e5e141a04789c112a29648af55514ebea548206e
viralkansodiya <viral@fosserp.com>
2025-08-04 11:16:19 +0530

    fix: service item rate calculation work before discount

--- custom_app/public/js/quotation.js
+++ custom_app/public/js/quotation.js
@@ -7,19 +7,40 @@ frappe.ui.form.on("Quotation", {
 frappe.ui.form.on("Quotation Item", {
     percentage:(frm, cdt, cdn)=>{
         let d = locals[cdt][cdn]
-        frappe.call({
-            method: "custom_app.doc_events.quotation.get_service_item_rate",
-            args : {
-                doc : frm.doc,
-                percentage : d.percentage,
-                item_code : d.item_code
-            },
-            callback:(r)=>{
-                if(r.message){
-                    frappe.model.set_value(cdt, cdn, "rate", r.message)
-                    frm.refresh_field("items")
-                }
-            }
-        })
-    }
+        calculate_service_item_rate(frm, cdt , cdn)
+    },
+    rate:(frm, cdt, cdn)=>{
+        let d = locals[cdt][cdn]
+        calculate_service_item_rate(frm, cdt , cdn)
+    },
+    qty:(frm, cdt, cdn)=>{
+        let d = locals[cdt][cdn]
+        calculate_service_item_rate(frm, cdt , cdn)
+    },
+    discount_percentage:(frm, cdt, cdn)=>{
+        let d = locals[cdt][cdn]
+        calculate_service_item_rate(frm, cdt , cdn)
+    },
+    discount_amount:(frm, cdt, cdn)=>{
+        let d = locals[cdt][cdn]
+        calculate_service_item_rate(frm, cdt , cdn)
+    },
+
 })
+function calculate_service_item_rate(frm, cdt, cdn){
+    let d = locals[cdt][cdn]
+    frappe.call({
+        method: "custom_app.doc_events.quotation.get_service_item_rate",
+        args : {
+            doc : frm.doc,
+            percentage : d.percentage,
+            item_code : d.item_code
+        },
+        callback:(r)=>{
+            if(r.message){
+                frappe.model.set_value(cdt, cdn, "rate", r.message)
+                frm.refresh_field("items")
+            }
+        }
+    })
+}
